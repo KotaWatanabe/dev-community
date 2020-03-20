1280b6204ae29ec10fa8294e5e9a683decf9996f
Kota Watanabe <kota3316@gmail.com>
2020-03-20 16:13:00 -0700

    link to register and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => 
+  <Router>
     <>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </section>
     </>
-
+  </Router>
 export default App;
