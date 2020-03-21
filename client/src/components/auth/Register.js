1ad58fe5e52e6cb57200625ea987f8efb98b04b8
Kota Watanabe <kota3316@gmail.com>
2020-03-21 12:10:31 -0700

    create login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -18,27 +18,9 @@ const Register = () => {
         if(password !== password2) {
             console.log('Password do not match')
         } else {
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try {
-                const config = {
-                    headers: {
-                        'Conetent-Type': 'application/json'
-                    }
-                }
-
-                const body = JSON.stringify(newUser);
-
-                const res = await axios.post("/api/users", body, config);
-                console.log(res.data);
-            } catch (err) {
-                console.error(err.message)
-            }
+            console.log('SUCCESS');
         }
-    }
+    };
     return (
         <>
             <h1 className="large text-primary">Sign Up</h1>
@@ -92,10 +74,10 @@ const Register = () => {
               <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-              Already have an account? <a href="login.html">Sign In</a>
+              Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </>
     )
-}
+};
 
 export default Register;
