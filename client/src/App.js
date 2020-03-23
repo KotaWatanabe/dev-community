f0a98d3374d20be49c9db382f5ee4f41211a99ef
Kota Watanabe <kota3316@gmail.com>
2020-03-22 17:28:46 -0700

    create store

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,12 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => 
+  <Provider store={store}>
   <Router>
     <>
       <Navbar />
@@ -19,4 +23,5 @@ const App = () =>
       </section>
     </>
   </Router>
+  </Provider>
 export default App;
