c2da8453492f021f02f16892f967941af80a1390
Kota Watanabe <kota3316@gmail.com>
2020-03-24 17:03:04 -0700

    protected route for dashboard

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -27,7 +27,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
     };
 
     if(isAuthenticated) {
-        return <Redirect to="/dashboadr" />
+        return <Redirect to="/dashboard" />
     }
     return (
         <>
