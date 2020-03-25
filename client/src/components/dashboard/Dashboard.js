ea81c4cc7d0a077b9ff2ad1f4cfd65c8fd6d84e2
Kota Watanabe <kota3316@gmail.com>
2020-03-25 15:14:00 -0700

    spinner component and dashboad auth

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({auth, profile, getCurrentProfile }) => {
+const Dashboard = ({
+    auth:{ user },
+    profile:{ profile, loading }, 
+    getCurrentProfile 
+}) => {
     useEffect(() => {
         getCurrentProfile()
     },[]);
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-};
+    return loading && profile === null ? <Spinner /> : 
+        <Fragment>
+          <h1 className="large text-primary">Dashboard</h1>
+          <p className="lead">
+          <i className="fas fa-user"></i> Welcome { user && user.name }
+          </p>
+          {profile !== null ? (
+            <Fragment>has</Fragment> 
+           ) : (
+            <Fragment>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to='/create-profile' className="btn btn-primary my-1">
+                Create Profile
+              </Link>
+            </Fragment>
+          )}
+        </Fragment>
+};          
 
 Dashboard.propTypes = {
     getCurrentProfile:PropTypes.func.isRequired,
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -7,6 +7,12 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth:{ isAuthenticated, loading }, logout }) => {
     const authLinks = (
       <ul>
+        <li>
+          <Link to="/dashboard">
+            <i className="fas fa-user"></i>{' '}
+            <span className="hide-sm">Dashboard</span> 
+          </Link>
+        </li>
         <li><a onClick={logout} href="#!">
           <i className="fas fa-sign-out-alt"></i>{' '}
             <span className="hide-sm">Logout</span> 
@@ -28,7 +34,7 @@ const Navbar = ({ auth:{ isAuthenticated, loading }, logout }) => {
         <h1>
           <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
         </h1>
-        {  !loading && (<>{ isAuthenticated ? authLinks : guestLinks }</>)}
+        {  !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
       </nav>
     )
 }
