90303626bf383390ec61006ce837ead93615f13f
Kota Watanabe <kota3316@gmail.com>
2020-03-29 15:24:31 -0700

    create profile by id component

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -38,7 +38,7 @@ const EditProfile = ({ profile: { profile, loading },createProfile, getCurrentPr
             youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
             instagram: loading || !profile.social.instagram ? '' : profile.social.instagram,
         })
-    }, [loading]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
