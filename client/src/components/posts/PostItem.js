ea2d58eac2b9c63bb0d8d18be81b44e2919d12c6
Kota Watanabe <kota3316@gmail.com>
2020-03-30 15:40:46 -0700

    like and unlike function

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
 
-const PostItem = ({auth, 
+const PostItem = ({addLike, removeLike, auth, 
     post: {
         _id,
         text,
@@ -34,13 +35,13 @@ const PostItem = ({auth,
              <p className="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button type="button" className="btn btn-light">
+            <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
               <i className="fas fa-thumbs-up">{' '}</i>
               <span>{likes.length > 0 && (
               <span>{likes.length}</span>
               )}</span>
             </button>
-            <button type="button" className="btn btn-light">
+            <button type="button" className="btn btn-light" onClick={() => removeLike(_id)}>
               <i className="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -69,4 +70,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps,{})(PostItem);
+export default connect(mapStateToProps,{ addLike, removeLike })(PostItem);
