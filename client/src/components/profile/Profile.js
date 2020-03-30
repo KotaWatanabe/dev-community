87affe2106fba5b264a811053b6428ee8470a40c
Kota Watanabe <kota3316@gmail.com>
2020-03-29 21:29:25 -0700

    ProfileTop and ProfileAbout components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ match,getProfileById, profile:{ profile, loading}, auth }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    },[getProfileById]);
+    },[getProfileById, match.params.id]);
 
     return (
         <Fragment>
@@ -18,6 +20,10 @@ const Profile = ({ match,getProfileById, profile:{ profile, loading}, auth }) =>
                     Back to Profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to ='/edit-profile' className='btn btn-daark'>Edit Profile</Link>)}
+                <div class="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
+                </div>
             </Fragment>}
         </Fragment>
     )
