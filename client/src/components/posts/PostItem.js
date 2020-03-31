6406352da2a433f29970b839219a8d4d963471af
Kota Watanabe <kota3316@gmail.com>
2020-03-31 11:17:39 -0700

    single post desplay

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -15,7 +15,9 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
         likes,
         comments,
         date
-}}) => 
+    },
+    showActions
+}) => 
     <div className="post bg-white p-1 my-1">
           <div>
             <Link to={`/profile/${user}`}>
@@ -34,7 +36,9 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
              <p className="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
+
+            {showActions && <Fragment>
+              <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
               <i className="fas fa-thumbs-up">{' '}</i>
               <span>{likes.length > 0 && (
               <span>{likes.length}</span>
@@ -43,7 +47,7 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
             <button type="button" className="btn btn-light" onClick={() => removeLike(_id)}>
               <i className="fas fa-thumbs-down"></i>
             </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
+            <Link to={`/posts/${_id}`} className="btn btn-primary">
               Discussion {' '}
               {comments.length > 0 && (
               <span className='comment-count'>{comments.length}</span>
@@ -58,8 +62,13 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
                     <i className="fas fa-times"></i>
                 </button>
             )}
+            </Fragment>}
           </div>
     </div>
+
+PostItem.defaultProps = {
+  showActions: true
+}
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
